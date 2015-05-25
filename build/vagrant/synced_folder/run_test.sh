e74e67376559b85cf687b9658805def250d1b68b
sefigiga <gigaS3f1nbar>
2015-05-25 13:16:28 +0300

    feat(tests): add system tests
    [CFY-2106] add vagrant automation

--- build/vagrant/synced_folder/run_test.sh
+++ build/vagrant/synced_folder/run_test.sh
@@ -2,7 +2,7 @@
 
 SYSTEM_TESTS_FOLDER=system-tests
 rm -rf $SYSTEM_TESTS_FOLDER || echo "folder does not exist"
-git clone https://github.com/cloudify-cosmo/cloudify-js.git $SYSTEM_TESTS_FOLDER
+git clone -b CFY-2106-system-tests https://github.com/cloudify-cosmo/cloudify-js.git $SYSTEM_TESTS_FOLDER
 cd $SYSTEM_TESTS_FOLDER
 
 
