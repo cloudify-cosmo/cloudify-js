e330e8d851623e2ca1afd414e27d464983c2bc7a
sefigiga <gigaS3f1nbar>
2015-05-26 11:46:20 +0300

    feat(tests): add system tests
    [CFY-2106] add vagrant automation

--- build/vagrant/synced_folder/setup_tests_env.sh
+++ build/vagrant/synced_folder/setup_tests_env.sh
@@ -1,6 +1,5 @@
-echo "uploading blueprints and creating deployments for tests"
-cfy blueprints publish-archive -l ../../../test/resources/testHelloWorld.tar.gz -b HelloWorld -n blueprint.yaml
-cfy blueprints publish-archive -l ../../../test/resources/cloudify-nodecellar-example.tar.gz -b nodecellar -n openstack-blueprint.yaml
+echo "uploading blueprints and creating deployments for cloudify-js tests"
+cfy blueprints publish-archive -l https://github.com/cloudify-cosmo/cloudify-js/tree/CFY-2106-system-tests/test/resources/testHelloWorld.tar.gz -b HelloWorld -n blueprint.yaml
+cfy blueprints publish-archive -l https://github.com/cloudify-cosmo/cloudify-js/tree/CFY-2106-system-tests/test/resources/cloudify-nodecellar-example.tar.gz -b nodecellar -n openstack-blueprint.yaml
 cfy deployments create -b HelloWorld -d HelloWorld
-cfy deployments create -b nodecellar -d nodecellar --inputs ../../../test/resources/cloudify-nodecellar-example-inputs.yaml
-
+cfy deployments create -b nodecellar -d nodecellar --inputs https://github.com/cloudify-cosmo/cloudify-js/tree/CFY-2106-system-tests/test/resources/cloudify-nodecellar-example-inputs.yaml
