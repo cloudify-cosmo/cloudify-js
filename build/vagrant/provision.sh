a909f1733c14813b7335fb6297b1e08e33e6d4d0
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2015-06-19 11:28:27 +0300

    chore(build): move to 3.3

--- build/vagrant/provision.sh
+++ build/vagrant/provision.sh
@@ -10,6 +10,6 @@ sudo npm -g install cloudify-cosmo/cloudify-installer --ignore-scripts
 export INSTALL_SYSTEM_TESTS_REQ=true
 export SYSTEM_TESTS_VIRTUAL_ENV=/tmp/cloudify-js-virtenv
 
-cloudify-installer run_script -s 3.2.0/vagrant_install_simple/script.sh
+cloudify-installer run_script -s 3.3.0/vagrant_install_simple/script.sh
 
 ./run_test.sh
