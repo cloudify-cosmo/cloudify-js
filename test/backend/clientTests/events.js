b21ac7935002d8ce5d9e97927465fbd5da01a6c1
guy-mograbi-at-gigaspaces <guym@gigaspaces.com>
2015-07-06 18:09:16 +0300

    fix: modify events test to match new signature

--- test/backend/clientTests/events.js
+++ test/backend/clientTests/events.js
@@ -10,7 +10,7 @@ describe('events:', function () {
             var executions = body;
             expect(executions).to.not.be.empty();
 
-            testClient.events.get(executions[0].id, 0, 100, false, function(err, response, body) {
+            testClient.events.get( { execution_id : executions[0].id }, function(err, response, body) {
                 expect(body).to.be.ok();
                 expect(response.statusCode).to.be(200);
 
